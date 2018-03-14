4581d0701e4c0acb87903f36dad733582935f1d3
kleytonsantos <kleytonandroid@gmail.com>
2018-03-14 14:26:25 -0300

    Resolvendo bug

--- app/src/main/java/com/thoughtworks/www/workshopmvvm/MainActivity.java
+++ app/src/main/java/com/thoughtworks/www/workshopmvvm/MainActivity.java
@@ -1,16 +1,14 @@
 package com.thoughtworks.www.workshopmvvm;
 
 import android.databinding.DataBindingUtil;
-import android.os.PersistableBundle;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.thoughtworks.www.workshopmvvm.databinding.ActivityMainBinding;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String VIEW_MODEL = "VIEW_MODEL";
     private MainActivityViewModel viewModel;
 
     @Override
@@ -18,24 +16,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        viewModel = getViewModel();
+        viewModel = new MainActivityViewModel(new NameProvider());
         binding.setViewModel(viewModel);
     }
-
-
-    private MainActivityViewModel getViewModel() {
-
-        ViewModelHolder<MainActivityViewModel> holder = (ViewModelHolder<MainActivityViewModel>) getSupportFragmentManager().findFragmentByTag(VIEW_MODEL);
-        if (!(holder != null && holder.getViewModel() != null)) {
-            MainActivityViewModel viewModel = new MainActivityViewModel(new NameProvider());
-            holder = ViewModelHolder.newInstance(viewModel);
-            getSupportFragmentManager().beginTransaction().add(holder, VIEW_MODEL).commit();
-        }
-
-        return holder.getViewModel();
-
-    }
-
-
 }
 
